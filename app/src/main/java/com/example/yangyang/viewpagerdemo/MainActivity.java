8a848bd9d67f4ac5f42f86f8b32f016a349e8209
杨杨 <yangyang.0926@bytedance.com>
2017-02-21 16:22:07 +0800

    防止多次实例化dialog

--- app/src/main/java/com/example/yangyang/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/example/yangyang/viewpagerdemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.yangyang.viewpagerdemo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,63 +20,57 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
     private int mWearCount = 16;
-    private HeadWearDialog headWearDialog;
+    private HeadWearDialog mDialog;
+    private IHeadwear mIHeadwear = new IHeadwear() {
+        @Override
+        public List<Headwear> getHeadwear() {
+            //初始化头饰图片数组
+            //获取 headwear 封装，目前仅封装了图片 uri 的信息，是否已经下载的信息。
+            ArrayList<Headwear> list = new ArrayList<>();
+            for (int i = 0; i < mWearCount; i++) {
+                Headwear headwear = new Headwear();
+                headwear.setUri("http://d.lanrentuku.com/down/png/1610/50space-elements/rocket-ship-2.png");
+                if (i % 3 == 0) headwear.setNotDownload(true);
+                list.add(headwear);
+            }
+            return list;
+        }
+
+        @Override
+        public void selectNoneHeadwear() {
+            //主线程中，取消头饰
+        }
+
+        @Override
+        public void selectHeadwear(Headwear headwear) {
+            //下载头饰
+            //完成下载
+            mDialog.finishDownload(headwear);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Fresco.initialize(this);
         final EditText mEditText = (EditText) findViewById(R.id.edit_wear_count);
+        //初始化dialog，注意不要重复初始化
+        mDialog = new HeadWearDialog(this, mIHeadwear);
+
         findViewById(R.id.btn_pop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String editStr = mEditText.getText().toString().trim();
                 if (editStr.length() > 0) mWearCount = Integer.parseInt(editStr);
-                showHeadwearDialog(MainActivity.this, new IHeadwear() {
-                    @Override
-                    public List<Headwear> getHeadwear() {
-                        //获取 headwear 封装，目前仅封装了图片 uri 的信息，是否已经下载的信息。
-                        List<Headwear> list = new ArrayList<>();
-                        for (int i = 0; i < mWearCount; i++) {
-                            Headwear headwear = new Headwear();
-                            headwear.setUri("http://d.lanrentuku.com/down/png/1610/50space-elements/rocket-ship-2.png");
-                            if (i % 3 == 0) headwear.setNotDownload(true);
-                            list.add(headwear);
-                        }
-                        return list;
-                    }
-
-                    @Override
-                    public void selectNoneHeadwear() {
-                        //主线程中，取消头饰
-                    }
-
-                    @Override
-                    public void selectHeadwear(Headwear headwear) {
-                        //下载头饰
-
-                        //完成下载
-                        headWearDialog.finishDownload(headwear);
-
-                    }
-
-
-
-
-                });
+                mDialog.show();
             }
         });
 
 
     }
 
-    //使用方法。
-    private void showHeadwearDialog(final Context context, IHeadwear iHeadwear) {
-        headWearDialog = new HeadWearDialog(context, iHeadwear);
-        //设置图片资源drawable数组。
-        headWearDialog.show();
 
-    }
+
 }
